d29d07df88b5ad5c4a4899ee9c114ebc98559cc6
Tylor Schafer <tylorschafer@gmail.com>
2020-02-16 16:24:11 -0700

    Refactor user cards into its own component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,6 @@
 <template>
   <div id="app" class="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
-    <ul class="w-full max-w-md">
-      <li v-for="user in users" 
-          :key="user.id"
-          class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
-        <div class="flex items-center">
-          <img class="w-10 h-10 rounded-full" :src="user.avatar" :alt="user.name">
-          <p class="ml-2 text-gray-700 font-semibold font-sans tracking-wide">{{user.name}}</p>
-        </div>
-        {{user.name}}
-        <div class="flex">
-          <button aria-label="Edit User"
-                  class="p-1 focus:outline-none focus:shadow-outline text-teal-500 hover:text-teal-600"
-                  @click="onEdit(user)"> 
-            <EditIcon/>
-          </button>
-          <button aria-label="Delete user"
-                  class="p-1 focus:outline-none focus:shadow-outline text-red-500 hover:text-red-600"
-                  @click="onDelete(user)">
-            <Trash2Icon/>
-          </button>
-        </div>
-      </li>
-    </ul>
+
   </div>
 </template>
 
@@ -51,18 +29,18 @@ export default {
           name: "Steve Jobs",
           avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
         }
-      ],
-      methods: {
-        onEdit(user) {
-          alert(`Editing ${user.name}`);
-        },
-        onDelete(user) {
-          alert(`Deleting ${user.name}`);
-        }
-      }
+      ]
+    }
+  },
+  methods: {
+    onEdit(user) {
+      alert(`Editing ${user.name}`);
+    },
+    onDelete(user) {
+      alert(`Deleting ${user.name}`);
     }
   }
-}
+};
 </script>
 
-
+                  
