deccc28da0f371e769c64f8edbafeae13f92285e
Tylor Schafer <tylorschafer@gmail.com>
2020-02-16 15:46:45 -0700

    Add basic user list with sample data

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,42 @@
 <template>
   <div id="app" class="min-h-screen w-screen bg-gray-200 flex items-center justify-center">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <ul class="w-full max-w-md">
+      <li v-for="user in users" 
+          :key="user.id"
+          class="p-4 mb-3 flex justify-between items-center bg-white shadow rounded-lg cursor-move">
+        {{user.name}}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import './assets/css/tailwind.css'
-
 export default {
   name: 'App',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      users: [
+        {
+          id: 1,
+          name: "Lady GaGa",
+          avatar: "https://pickaface.net/gallery/avatar/judycsmosh521a551b604bf.png"
+        },
+        {
+          id: 2,
+          name: "Johnny Depp",
+          avatar: "https://pickaface.net/gallery/avatar/gs315535348ce076c6.png"
+        },
+        {
+          id: 3,
+          name: "Steve Jobs",
+          avatar: "https://pickaface.net/gallery/avatar/Opi51c74d0125fd4.png"
+        }
+      ]
+    }
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+
